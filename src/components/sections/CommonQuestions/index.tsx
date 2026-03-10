4727b52ed96ee09fdffe265c2e496d027895766e
Humberto Monteiro <humbertoaraujox@icloud.com>
2026-03-10 14:35:13 -0300

    ajuste mesnsagem whatsapp

--- src/components/sections/CommonQuestions/index.tsx
+++ src/components/sections/CommonQuestions/index.tsx
@@ -142,7 +142,7 @@ export default function CommonQuestions() {
     setOpenQuestion(openQuestion === id ? null : id);
   };
 
-  const whatsappLink = `https://api.whatsapp.com/send?phone=55${event.contacts.whatsapp}&text=Ol%C3%A1%2C%20equipe%20autismoAutismo!%20Estou%20interessado(a)%20no%20evento%20e%20gostaria%20de%20obter%20mais%20informa%C3%A7%C3%B5es.%20%0A%0APoderiam%20me%20ajudar%20com%20detalhes%20sobre%20inscri%C3%A7%C3%B5es%2C%20programa%C3%A7%C3%A3o%2C%20palestrantes%20e%20outras%20informa%C3%A7%C3%B5es%20relevantes%3F%20Obrigado(a)!%0A%0A`;
+  const whatsappLink = `https://api.whatsapp.com/send?phone=55${event.contacts.whatsapp}&text=Ol%C3%A1!%20Estou%20interessado(a)%20no%20evento%20e%20gostaria%20de%20obter%20mais%20informa%C3%A7%C3%B5es.%20%0A%0APoderiam%20me%20ajudar%20com%20detalhes%20sobre%20inscri%C3%A7%C3%B5es%2C%20programa%C3%A7%C3%A3o%2C%20palestrantes%20e%20outras%20informa%C3%A7%C3%B5es%20relevantes%3F%20Obrigado(a)!%0A%0A`;
 
   return (
     <section className={styles.section} id="faq">
